939afa55de42c5d1532e902d142f5b175ad58d3a
Zard Studios <gameroom.fede.ale@gmail.com>
2026-05-26 19:55:40 +0200

    Release WineCord 0.1.10

--- Formula/winecord.rb
+++ Formula/winecord.rb
@@ -1,8 +1,8 @@
 class Winecord < Formula
   desc "Discord Rich Presence bridge for Windows games running through Wine on macOS"
   homepage "https://github.com/Zard-Studios/WineCord"
-  url "https://raw.githubusercontent.com/Zard-Studios/homebrew-tap/main/releases/winecord-0.1.9-macos-universal.tar.gz"
-  sha256 "fe5bd2c4df34a625deadbfc05aceb8e2d5e4dd362f4a2e8137a0fd1acaa91ea6"
+  url "https://raw.githubusercontent.com/Zard-Studios/homebrew-tap/main/releases/winecord-0.1.10-macos-universal.tar.gz"
+  sha256 "fd1aa184362c1bffdf69e0b754296c507a8ef99b1132e9f820039c6364ec6fc6"
   license "MIT"
 
   def install
